de413b27731a918fcc6ea5d94692e4b05bb80df9
jmp1234 <johnnypham944@yahoo.com>
2019-03-29 16:51:45 -0700

    added basic product list endpoint wrote query for product list added join for images pushed images into array fixed price to be number converted data to json

--- public/api/getproducts.php
+++ public/api/getproducts.php
@@ -2,7 +2,54 @@
 
 require_once('mysqlconnect.php');
 
+$query = "SELECT p.`id`, p.`name`, p.`price`,
+    i.`url` AS `images`
+  FROM `products` AS p
+  JOIN `images` AS i
+    ON p.`id` = i.`products_id`
+  ORDER BY p.`id`
+";
+
+
+$result = mysqli_query($conn, $query);
+
+$data = [];
+
+while($row = mysqli_fetch_assoc($result)) {
+  $currentID = $row['id'];
+  if( isset( $data[$currentID] ) ) {
+    // $data[$row['id']] ['images'][] = $row['images'];
+    // $currentID = $row['id'];
+    $image = $row['images'];
+    $data[$currentID]['images'][] = $image;
+    // array_push($data[$currentID]['images'], $image);
+  } else {
+    $image = $row['images'];
+    unset($row['images']);
+    $row['images'] = [];
+    //array_push($row['images'], $image);
+    $row['images'][] = $image;
+
+    $row['price'] = intval($row['price']);
+
+    $data[$currentID] = $row;
+  }
+}
+
+$pureData = [];
+foreach($data as $value){
+  $pureData[] = $value;
+}
+
+$output = [
+  'success'=>true,
+  'products'=> $pureData
+];
+
+$json_output = json_encode($output);
+
+print($json_output);
 
 
 
- ?>
+?>
