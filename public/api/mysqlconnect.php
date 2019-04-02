a8b89de6963a2e43c50201bfb9dbd8e80d566402
jmp1234 <johnnypham944@yahoo.com>
2019-04-02 11:32:55 -0700

    Client setup in progress

---
+++ public/api/mysqlconnect.php
@@ -0,0 +1,9 @@
+<?php
+
+// procedural style
+$conn = mysqli_connect('localhost', 'root', 'root', '2.19wickedsales', 8889);
+
+//oop style
+$db = new mysqli('localhost', 'root', 'root', '2.19wickedsales', 8889)
+
+ ?>
