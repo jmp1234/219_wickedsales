9418821f9553694ab716e1ebb7967afdedce1b53
jmp1234 <johnnypham944@yahoo.com>
2019-04-11 12:25:13 -0700

    added basic authentication server hashed password checked database made and shared token added connect entry

--- public/api/login.php
+++ public/api/login.php
@@ -38,10 +38,42 @@ if(mysqli_num_rows($result) !== 1) {
   throw new Exception('invalid username or password');
 }
 
+//no error:
+
 $data = mysqli_fetch_assoc($result);
+
+$token = $email . $data['id'] . microtime(); //microtime gives you number of milliseconds since epoch
+$token = sha1($token);
+
+$connect_query = "INSERT INTO `user_connections` SET
+  `token` = '$token',
+  `users_id` = {$data['id']},
+  `created` = NOW(),
+  `ip_address` = '{$_SERVER['REMOTE_ADDR']}'
+
+";
+
+
+$connect_result = mysqli_query($conn, $connect_query);
+
+if(!$connect_result) {
+  throw new Exception(mysqli_error($conn));
+}
+
+if(mysqli_affected_rows($conn) !==1) {
+  throw new Exception('could not log you in: connection not saved');
+}
+
+$_SESSION['user_data'] = [
+  'id' => $data['id'],
+  'username'=>$data['name'],
+  'token'=>$token
+
+];
 
 $output['success'] = true;
 $output['username'] = $data['name'];
+$output['token'] = $token;
 
 $json_output = json_encode($output);
 
