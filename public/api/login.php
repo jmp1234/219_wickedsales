c45174bfa566ef13b147b0d5f6f672b50b65f624
jmp1234 <johnnypham944@yahoo.com>
2019-04-11 13:03:31 -0700

    updated to axios format updated login endpoint to use json

--- public/api/login.php
+++ public/api/login.php
@@ -9,20 +9,31 @@ $output = [
   'success'=> false
 ];
 
-if(empty($_POST['email'])) {
+$json_input = file_get_contents("php://input"); //raw input source(body) for request.  It gets the raw string for the body
+
+$input = json_decode($json_input, true); //true turns all objects into associative arrays
+
+// if(empty($_POST['email'])) {
+//   throw new Exception('email is a required value');
+// }
+//
+// if(empty($_POST['password'])) {
+//   throw new Exception('password is a required value');
+// }
+if(empty($input['email'])) {
   throw new Exception('email is a required value');
 }
 
-if(empty($_POST['password'])) {
+if(empty($input['password'])) {
   throw new Exception('password is a required value');
 }
 
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$email = $input['email'];
+$password = $input['password'];
 $hashedPassword = sha1($password);
 
-unset($_POST['password']);
+unset($input['password']);
 
 $query = "SELECT `id`, `name` FROM `users`
             WHERE `email` = '$email' AND `password` = '$hashedPassword'
