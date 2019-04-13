31ea00db688a002241cfca2806fffce4fcace834
jmp1234 <johnnypham944@yahoo.com>
2019-04-12 18:09:46 -0700

    made prepare statements in login.php

--- public/api/login.php
+++ public/api/login.php
@@ -39,10 +39,20 @@ $hashedPassword = sha1($password);
 unset($input['password']);
 
 $query = "SELECT `id`, `name` FROM `users`
-            WHERE `email` = '$email' AND `password` = '$hashedPassword'
+            WHERE `email` = ? AND `password` = ?
 ";
 
-$result = mysqli_query($conn, $query); //mysqli object: the reference to the dataset for that query
+//1)send the safe query to the db
+$statement = mysqli_prepare($conn, $query);
+//2)send the dangerous data to the DB
+mysqli_stmt_bind_param($statement, 'ss', $email, $hashedPassword);
+//3)tell the DB to mix the query and the data
+mysqli_stmt_execute($statement);
+//4)get the result pointer for the prepared query statement's data
+$result = mysqli_stmt_get_result($statement);
+//now you can use the result var as normal
+//not needed anymore because of prepared statement
+// $result = mysqli_query($conn, $query); //mysqli object: the reference to the dataset for that query
 
 if(!$result) {
   throw new Exception(mysqli_error($conn));
