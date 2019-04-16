f1884f69c32109ceb2c881db13524d0a1435f584
jmp1234 <johnnypham944@yahoo.com>
2019-04-15 18:03:34 -0700

    created sign in form

--- src/components/account/sign_in/sign_in_form.js
+++ src/components/account/sign_in/sign_in_form.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import Input from '../../general/input';
 
 const SignInForm = props => {
+  console.log(props)
+  const {handleSubmit, signIn} = props;
 
   return (
-    <form>
-      <h1>form goes here</h1>
+    <form onSubmit={handleSubmit(signIn)} >
+
+      <div className="row">
+        <Field  col="s12" id="Dog" name="email" component={Input} label="Email"/>
+      </div>
+      <div className="row">
+        <Field col="s12" id="password" name="password" component={Input} type="password" label="Password"/>
+      </div>
+
+
+      <div className="row">
+        <div className="col s12 right-align">
+          <button className="btn blue">Sign In</button>
+        </div>
+      </div>
     </form>
   )
 }
